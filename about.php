dd6c2c05c192b77c24e63eac8d56d715d253904e
Ankush Sharma <ankushsharma11110@gmail.com>
2019-08-09 13:01:32 +0530

    Code

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css" />
+</head>
+<body>
+    <div class="container">
+        <?php include('menu.php'); ?>
+        <div class="row">
+            <div class="col-md-12">
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt aliquam, ab, amet numquam eum nisi, voluptatum dignissimos ipsum omnis sequi harum nobis! Commodi accusantium veritatis eligendi voluptatem minus, explicabo officiis?</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 bg-info">
+                <p>Footer Content</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
